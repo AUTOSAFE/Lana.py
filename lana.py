10015a19a8a1c46090f785f8af16c519cc3abb19
AUTOSAFE <107992265+AUTOSAFE@users.noreply.github.com>
2022-07-21 21:42:45 +0800

    Update lana.py

--- lana.py
+++ lana.py
@@ -1,57 +1,3 @@
-import requests, threading, datetime, sys, os, time
-
-def main():
-	global auth, maxerr, api, pos, dely
-	os.system('cls' if os.name == 'nt' else 'clear')
-	print(f"Script By Zexxy & Lana.")
-	print(f"")
-	print(f"Kalau stuck tunggu aja")
-	api = "kitkabackend.eastus.cloudapp.azure.com:5010"
-	auth = str(input("Auth Key: "))
-	pos = int(input("""
-0 = Round 1 (Eliminated)
-1 = Round 2 (Eliminated)
-2 = Round 3 (Eliminated)
-3 = Round 3 (Winner)
-Input: """))
-	dely = float(input("\nCrown ( input delay): "))
-	thr = int(input("\nThreads ( Default '1' | Massukkan Berapa crown yang kamu mau ): "))
-	print("===============STARTING==============="*1)
-	for _ in range(thr):
-	        threading.Thread(target=s).start()
-
-def s():
-        global maxerr
-        while True:
-                dt = datetime.datetime.now()
-                try:
-                        headers = {
-                            'authorization': auth,
-                            'use_response_compression': 'true',
-                            'Accept-Encoding': 'gzip',
-                            'Host': api,
-                            'Connection': None,
-                            'User-Agent': None,
-                        }
-                        response = requests.get(f'http://{api}/round/finishv2/{pos}', headers=headers)
-                        if response.status_code == 200:
-                                negara = response.text.split('"Country":')[1].split(',')[0]
-                                nama = response.text.split('"Username":')[1].split(',')[0]
-                                trophy = response.text.split('"SkillRating":')[1].split(',')[0]
-                                crown = response.text.split('"Crowns":')[1].split(',')[0]
-                                sys.stdout.write(f"\r[{dt.hour}:{dt.minute}:{dt.second}] {negara} | Username: {nama} | Trophy: {trophy} | Crowns: {crown}")
-                                sys.stdout.flush()
-                        elif response.status_code == 403 and response.text == "BANNED":
-                                print(f"[{dt.hour}:{dt.minute}:{dt.second}] Auth Expired Buat Baru lagi!")
-                                break
-                                sys.exit(0)
-                        elif response.text == "SERVER_ERROR":
-                                continue
-                        else:
-                                print(f"[{response.status_code}] ini bukan expired tunggu")
-                        if dely > 0: time.sleep(dely)
-                except Exception as e:
-                        pass
-
-if __name__ == "__main__":
-	main()
+jhު笶a杊xj׭g,)uj)ڕfzڦ*hץʋ,^l,ץ%y)}'+Aɗ"ڝk{_򚕫$ib+diy-%ZntF-).ʚ,{b.D)^wThwvXvyݑIb)ڵwFwuw"znץhjاg
+0)^)ӆv}Sˤzi|	隺ԓ4m_➶akyb杵z,uZjYy:y]j׭gj׭gkujajب{(޲Ȩxz~w(v)8-jyrب'yK z{Mwzhǫz zb.ߊx!y^^i׫'zhǬn(uwjzhǭ{lX
+bIڙ޲'^)+TjgbKkr){mb)"TZ)e'){mb*{5bK2]p+^~mv٢^vrݝ,zڙڙ뢘r) 	r0̬h^'zhǬn(u4ݩݭ){mM:k{_vhm)m(.1*tVjK2bG޲'^ԄEQDQ9(ا祱k{_){֭ܡע&Ƙyn.^--gur{1qmڱZȟڙ
